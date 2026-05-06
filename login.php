df4ee8db6e508728ab89ea7c68b04320acc8860f
105538218 <105538218@student.swin.edu.au>
2026-05-06 06:42:27 +0300

    done lab 8

--- login.php
+++ login.php
@@ -1,10 +1,19 @@
-<form method="post" action="process.php">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'header.inc'; ?>
+</head>
+
+<body>
+    <h2>Login</h2>
+    <form method="post" action ="process.php">
     <label for="username">Username:</label>
     <input type="text" name="username" required><br>
-    
+
     <label for="password">Password:</label>
     <input type="password" name="password" required><br>
 
-    <input type="hidden" name="token" value="abc123"> 
+    <input type="hidden" name="token" value="L105538218">
     <input type="submit" value="Login">
     </form>
+</body>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.inc'; ?>
+
+<body>
+<?php
+session_start();
+if(isset($_SESSION['user'])) {
+  echo "Welcome, " .$_SESSION['user'] . "!";
+  } else {
+    header('Location: login.html');
+  }
+?>
+</body>
+</html>
